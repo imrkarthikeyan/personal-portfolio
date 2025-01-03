01186e0e9f07d2a7e4c64345e745c613e32b14d4
Karthikeyan R <168166270+imrkarthikeyan@users.noreply.github.com>
2025-01-03 10:29:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const contactme=document.getElementById("contactme");
+    const con=document.getElementById("con");
+    const mobile=document.getElementById("mobile");
+    const email=document.getElementById("email");
+    const btn=document.getElementById("btn");
+    contactme.addEventListener("click",function(){
+        con.style.display="block";
+    });
+    mobile.addEventListener("click", function(){
+        window.location.href="tel:+91 9025758149";
+    });
+    email.addEventListener("click",function(){
+        window.location.href="mailto:rkarthikeyan578@gmail.com";
+    });
+    btn.addEventListener("click",function(){
+        window.open("https://drive.google.com/file/d/1f-M5LHIX3MkTb4wqL5n-SuQfMZpxRebn/view?usp=sharing","_blank");
+    });
+});
